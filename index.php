de9e23cda05c2f4bd9911b126d18e3100b687a9c
SierraKill <yoleiru@gmail.com>
2022-10-20 08:38:31 -0600

    cambios

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+    <div class="container">
+
+    <section>
+        <div class="row justify-content-md-center align-items-center">
+            <div class="col-md-6 col-sm-12">
+                <form>
+           <h1 class="text-center">
+               Acceso al Panel
+           </h1>
+           
+                <label>
+                     Correo Electronico
+                </label>
+                <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                      <span class="input-group-text" id="basic-addon1">@</span>
+                    </div>
+                 <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                 <label>
+                Contraseña
+                 </label>
+                 <div class="input-group mb-3">
+                 <div class="input-group-prepend">
+                      <span class="input-group-text" id="basic-addon1">@</span>
+                    </div>
+                 <input type="password" class="form-control" placeholder="********" aria-label="password" aria-describedby="basic-addon1">
+                </div>
+                <button onclick="location.href=../Productos/products.php" type="button" class="btn btn-secondary btn-lg btn-block col-sm-12">
+                    ACCEDER
+                </button>
+
+
+                </form>
+            </div>
+        </div>
+    </section>
+    
+        
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
